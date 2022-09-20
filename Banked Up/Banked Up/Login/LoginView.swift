2af7c7fc0a2528fe14c5217637714a6d78395b0c
Jojo <jsphglmr@gmail.com>
2022-09-19 21:42:37 -0700

    new file setup

---
+++ Banked Up/Banked Up/Login/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  Banked Up
+//
+//  Created by Joseph Gilmore on 9/19/22.
+//
+
+import Foundation
---
+++ Banked Up/Banked Up/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banked Up
+//
+//  Created by Joseph Gilmore on 9/19/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
