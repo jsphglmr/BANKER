6a987a0efc7490ef5bb9b68790bd585a319198f7
Jojo <jsphglmr@gmail.com>
2022-09-20 22:12:12 -0700

    onboarding vc added

--- Banked Up/Banked Up/Onboarding/OnboardingViewController.swift
+++ Banked Up/Banked Up/Onboarding/OnboardingViewController.swift
@@ -5,4 +5,68 @@
 //  Created by Joseph Gilmore on 9/20/22.
 //
 
-import Foundation
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let image = UIImageView()
+    let label = UILabel()
+    
+    var imageName: String
+    var titleText: String
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+    
+    init(imageName: String, titleText: String) {
+        self.imageName = imageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension OnboardingViewController {
+    func style() {
+        view.backgroundColor = .systemBackground
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 24
+        
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
+        image.image = UIImage(named: imageName)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = titleText
+        label.font = UIFont.preferredFont(forTextStyle: .title2)
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(image)
+        stackView.addArrangedSubview(label)
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            image.heightAnchor.constraint(equalToConstant: 350),
+            image.widthAnchor.constraint(equalToConstant: 350),
+            
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
