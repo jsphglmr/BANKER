7d95f1d533204272ab0a6c20dd09edfc1bad221c
Jojo <jsphglmr@gmail.com>
2022-10-30 21:42:25 -0700

    custom cell for transfer vc

--- BANKER/BANKER/AccountTransfer/Cells/AccountTransferCell.swift
+++ BANKER/BANKER/AccountTransfer/Cells/AccountTransferCell.swift
@@ -7,8 +7,145 @@
 
 import UIKit
 
-class AccountTransferCell: UITableViewCell {
-    
-    static let reuseID = "AccountTransferCell"
+    enum TransferDestination: String, Codable {
+        case To
+        case From
+    }
 
-}
+    class AccountTransferCell: UITableViewCell {
+        
+        struct ViewModel {
+            let accountType: TransferDestination
+            let accountName: String
+            let balance: Decimal
+            
+            var balanceAsAttributedString: NSAttributedString {
+                return CurrencyFormatter().makeAttributedCurrency(balance)
+            }
+        }
+        
+        let viewModel: ViewModel? = nil
+        
+        let typeLabel = UILabel()
+        let underlineView = UIView()
+        let nameLabel = UILabel()
+        
+        let balanceStackView = UIStackView()
+        let balanceLabel = UILabel()
+        let balanceAmountLabel = UILabel()
+        
+        let chevronImageVIew = UIImageView()
+        
+        static let reuseID = "AccountTransferCell"
+        static let rowHeight: CGFloat = 112
+        
+        override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            setup()
+            layout()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+
+    extension AccountTransferCell {
+        private func setup() {
+            typeLabel.translatesAutoresizingMaskIntoConstraints = false
+            typeLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+            typeLabel.adjustsFontForContentSizeCategory = true
+            typeLabel.text = "Account Type"
+            
+            underlineView.translatesAutoresizingMaskIntoConstraints = false
+            underlineView.backgroundColor = appColor
+            
+            nameLabel.translatesAutoresizingMaskIntoConstraints = false
+            nameLabel.font = UIFont.preferredFont(forTextStyle: .body)
+            nameLabel.adjustsFontSizeToFitWidth = true
+            nameLabel.text = "Account Name"
+            
+            balanceStackView.translatesAutoresizingMaskIntoConstraints = false
+            balanceStackView.axis = .vertical
+            balanceStackView.spacing = 0
+            
+            balanceLabel.translatesAutoresizingMaskIntoConstraints = false
+            balanceLabel.font = UIFont.preferredFont(forTextStyle: .body)
+            balanceLabel.textAlignment = .right
+            balanceLabel.adjustsFontSizeToFitWidth = true
+            balanceLabel.text = "Some balance"
+            
+            balanceAmountLabel.translatesAutoresizingMaskIntoConstraints = false
+            balanceAmountLabel.textAlignment = .right
+            balanceAmountLabel.attributedText = makeFormattedBalance(dollars: "XXX,XXX", cents: "XX")
+            
+            let chevronImage = UIImage(systemName: "chevron.right")!.withTintColor(appColor, renderingMode: .alwaysOriginal)
+            chevronImageVIew.translatesAutoresizingMaskIntoConstraints = false
+            chevronImageVIew.image = chevronImage
+            
+            contentView.addSubview(typeLabel)
+            contentView.addSubview(underlineView)
+            contentView.addSubview(nameLabel)
+            
+            balanceStackView.addArrangedSubview(balanceLabel)
+            balanceStackView.addArrangedSubview(balanceAmountLabel)
+            contentView.addSubview(balanceStackView)
+            contentView.addSubview(chevronImageVIew)
+            
+        }
+        
+        private func layout() {
+            NSLayoutConstraint.activate([
+                typeLabel.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 2),
+                typeLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+                
+                underlineView.topAnchor.constraint(equalToSystemSpacingBelow: typeLabel.bottomAnchor, multiplier: 1),
+                underlineView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+                underlineView.widthAnchor.constraint(equalToConstant: 60),
+                underlineView.heightAnchor.constraint(equalToConstant: 4),
+                
+                nameLabel.topAnchor.constraint(equalToSystemSpacingBelow: underlineView.bottomAnchor, multiplier: 2),
+                nameLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+                
+                balanceStackView.topAnchor.constraint(equalToSystemSpacingBelow: underlineView.bottomAnchor, multiplier: 0),
+                balanceStackView.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 4),
+                trailingAnchor.constraint(equalToSystemSpacingAfter: balanceStackView.trailingAnchor, multiplier: 4),
+                
+                chevronImageVIew.topAnchor.constraint(equalToSystemSpacingBelow: underlineView.bottomAnchor, multiplier: 1),
+                trailingAnchor.constraint(equalToSystemSpacingAfter: chevronImageVIew.trailingAnchor, multiplier: 1)
+            ])
+        }
+        
+        private func makeFormattedBalance(dollars: String, cents: String) -> NSMutableAttributedString {
+            let dollarSignAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .callout), .baselineOffset: 8]
+            let dollarAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .title1)]
+            let centAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .footnote), .baselineOffset: 8]
+            
+            let rootString = NSMutableAttributedString(string: "$", attributes: dollarSignAttributes)
+            let dollarString = NSAttributedString(string: dollars, attributes: dollarAttributes)
+            let centString = NSAttributedString(string: cents, attributes: centAttributes)
+            
+            rootString.append(dollarString)
+            rootString.append(centString)
+            
+            return rootString
+        }
+    }
+
+    extension AccountTransferCell {
+        func configure(with vm: ViewModel) {
+            typeLabel.text = vm.accountType.rawValue
+            nameLabel.text = vm.accountName
+            balanceLabel.text = "Balance"
+
+            balanceAmountLabel.attributedText = vm.balanceAsAttributedString
+            
+            switch vm.accountType {
+            case .To:
+                underlineView.backgroundColor = .systemRed
+            case .From:
+                underlineView.backgroundColor = .systemBlue
+            }
+        }
+    }
+
